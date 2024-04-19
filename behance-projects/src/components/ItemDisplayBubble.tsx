bf1f1c33375a80a3e977ef4dac77d020e9548dae
Kayvia Harriott <55689354+KayviaHarriott@users.noreply.github.com>
2024-04-19 17:24:26 -0500

    Added greyscale styling to item display bubble and layout for component, installed feather icons and ant design icons

--- behance-projects/src/components/ItemDisplayBubble.tsx
+++ behance-projects/src/components/ItemDisplayBubble.tsx
@@ -1,17 +1,69 @@
+import { Box, Button } from "@mui/material";
 import { FC } from "react";
+import { BehanceSquareFilled, LinkOutlined } from "@ant-design/icons";
 
-interface ItemDisplayBubbleProps{
-    title: string;
-    description: string;
-    url: string;
+interface ItemDisplayBubbleProps {
+  title: string;
+  description: string;
+  url: string;
 }
 
-export const ItemDisplayBubbleProps: FC<ItemDisplayBubbleProps> = ({title, description, url}) => {
-    return(
-        <div>
-            <p>{title}</p>
-            <p>{description}</p>
-            <p>{url}</p>
-        </div>
-    )
-}
+export const ItemDisplayBubbleProps: FC<ItemDisplayBubbleProps> = ({
+  title,
+  description,
+  url,
+}) => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "white",
+        p: 2,
+        maxWidth: "400px",
+        boxShadow: "4px 4px 10px 1px rgba(0,0,0,0.1)",
+        borderRadius: 2,
+        display: "flex",
+        flexDirection: "column",
+        gap: 1,
+      }}
+    >
+      <p className="font-bold">{title}</p>
+      <p className="text-[14px] text-[gray]">{description}</p>
+      <div className="flex items-center gap-1 pt-2">
+        <BehanceSquareFilled />
+        <p className="text-[14px]">John Doe </p>
+      </div>
+      <div className="flex gap-1 pt-4">
+        <Button
+          sx={{
+            width: "100%",
+            boxShadow: "none",
+            textTransform: "none",
+            backgroundColor: "black",
+          }}
+          href={url}
+          target="_blank"
+          variant="contained"
+        >
+          View Project
+        </Button>
+        <Button
+          sx={{
+            width: "100%",
+            textTransform: "none",
+            color: "black",
+            borderColor: "black",
+            display: "flex",
+            gap: 0.5,
+            alignContent: "center",
+            alignItems: "center",
+          }}
+          href={url}
+          target="_blank"
+          variant="text"
+        >
+          View Repo <LinkOutlined />
+        </Button>
+      </div>
+    </Box>
+  );
+};
--- behance-projects/src/pages/LandingPage.tsx
+++ behance-projects/src/pages/LandingPage.tsx
@@ -4,12 +4,16 @@ export const LandingPage = () => {
   const projectsList = [
     {
       title: "Finance Management Landing Page",
-      description: "",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Nam at augue velit. Etiam porttitor, augue non venenatis aliquam, 
+        sem massa facilisis neque.`,
       url: "",
     },
     {
       title: "Project Management SAAS App Landing Page",
-      description: "",
+      description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Nam at augue velit. Etiam porttitor, augue non venenatis 
+        aliquam, sem massa facilisis neque`,
       url: "",
     },
   ];
@@ -17,14 +21,16 @@ export const LandingPage = () => {
     <div>
       <p>Kayvia Harriott</p>
       <p>behance Projects</p>
-      {projectsList.map((item, index) => (
-        <ItemDisplayBubbleProps
-          key={index}
-          title={item.title}
-          description={item.description}
-          url={item.url}
-        />
-      ))}
+      <div className="flex gap-2">
+        {projectsList.map((item, index) => (
+          <ItemDisplayBubbleProps
+            key={index}
+            title={item.title}
+            description={item.description}
+            url={item.url}
+          />
+        ))}
+      </div>
     </div>
   );
 };
